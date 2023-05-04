fe6966095d749316cd2d05b4116b831ac0ec9362
minhlong149 <minhlong9514@gmail.com>
2023-05-04 15:17:21 +0700

    Integrating Socket.IO

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,10 +1,20 @@
-import { useState } from 'react'
+import { useState, useEffect } from "react";
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import { socket } from "./socket";
+
 function App() {
   const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    socket.connect();
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
 
   return (
     <>
--- server/index.js
+++ server/index.js
@@ -3,6 +3,21 @@ const app = express();
 const http = require("http");
 const server = http.createServer(app);
 
+const { Server } = require("socket.io");
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`user ${socket.id} connected`);
+
+  socket.on("disconnect", () => {
+    console.log(`user ${socket.id} disconnected`);
+  });
+});
+
 app.get("/", (req, res) => {
   res.send("<h1>Hello world</h1>");
 });
