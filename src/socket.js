3971f60b510cd517f19c71589f6ef6f4fc3855de
minhlong149 <minhlong9514@gmail.com>
2023-05-08 15:58:06 +0700

    Remove url connect

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,3 @@
 import { io } from "socket.io-client";
 
-const URL = "http://localhost:3000";
-
-export const socket = io(URL, { autoConnect: false });
+export const socket = io({ autoConnect: false });
